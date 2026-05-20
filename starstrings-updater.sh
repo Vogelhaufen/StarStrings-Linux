c382fd06f02ff32a2b3c7267758e4c4f53e32336
This is my GarbageHub. <gh@killingonsight.de>
2026-05-20 07:16:17 +0200

    Update starstrings-updater.sh

--- starstrings-updater.sh
+++ starstrings-updater.sh
@@ -5,6 +5,10 @@ REPO_URL="https://github.com/MrKraken/StarStrings.git"
 BASE_DIR="$HOME/Games/star-citizen/drive_c/Program Files/Roberts Space Industries/StarCitizen/LIVE"
 BRANCH="master"
 
+TARGET_FILE="Data/Localization/english/global.ini"
+USER_CFG="USER.cfg"
+LANG_LINE='g_language = english'
+
 echo "== StarStrings updater =="
 
 cd "$BASE_DIR"
@@ -14,16 +18,26 @@ if [ ! -d ".git" ]; then
 
     git init
     git remote add origin "$REPO_URL"
+fi
 
-    git fetch origin
-    git checkout -b "$BRANCH" "origin/$BRANCH"
+echo "Fetching latest files..."
+git fetch origin "$BRANCH"
+
+echo "Downloading $TARGET_FILE ..."
+git checkout "origin/$BRANCH" -- "$TARGET_FILE"
+
+echo "Checking USER.cfg ..."
+
+if [ ! -f "$USER_CFG" ]; then
+    echo "$USER_CFG not found, creating it..."
+    touch "$USER_CFG"
+fi
+
+if grep -Fxq "$LANG_LINE" "$USER_CFG"; then
+    echo "Language setting already present."
 else
-    echo "Updating repository..."
-
-    git fetch origin
-
-    # update everything except USER.cfg
-    git checkout "origin/$BRANCH" -- . ':!USER.cfg'
+    echo "Adding language setting to $USER_CFG ..."
+    echo "$LANG_LINE" >> "$USER_CFG"
 fi
 
 echo "Done."
